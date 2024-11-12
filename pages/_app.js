3fe48b0a5cd41cba0b9bf96a1359098501fdb5f2
JoeTheBest <jewelshahi10@gmail.com>
2024-11-12 19:50:57 +0200

    adding random color generator

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Color Gradient Generator</title>
+        <meta author="Jewel Shahi" content="This is my another React.js/Next.js project" />
+        <link rel="icon" href="/colorgradient.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
